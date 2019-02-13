d199649dc6e4a4a452699dc5a032cd75c90301ca
rafael vianna <rafaelvianna@outlook.com>
2019-02-12 23:47:57 -0200

    init project

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$('.hide-show').on('click', function(){
-  $('.opened').toggleClass('hide');
-  $('.content-container').toggleClass('biggest-container');
-});
-
-$('.menu').on('click', function(){
-  $('.opened').toggleClass('active');
-});
