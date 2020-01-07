a3778b546025c9a73e409adec06a5b162bcde88a
rafael2vianna <rafael.vianna@ingresso.com>
2020-01-07 16:29:02 -0300

    pwa test

---
+++ pwa/precache-manifest.7ddf2687da5041228ebbbd28f1e3bb7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15667d6c89907f948778fefb43988179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21bf09a04112989cc872",
+    "url": "/static/js/2.9b23d7c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9b23d7c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e3ec74522e2c37168e4",
+    "url": "/static/js/main.a73e3df0.chunk.js"
+  },
+  {
+    "revision": "7ff7d8859bf2a96b144a",
+    "url": "/static/js/runtime-main.fd624ff0.js"
+  }
+]);
